4134b6057650222f166a497d47bb721f83c19ae6
sjmc11 <sjmc11@gmail.com>
2025-12-08 21:54:03 +0000

    Page buidler submodule

---
+++ blocks/divider/Divider.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="page-container">
+      <hr class="border-t-2 border-gray-200" />
+   </div>
+</template>
+
+<script lang="ts" setup>
+import { defineOptions, defineProps } from "vue";
+
+defineOptions({
+   label: "Divider",
+});
+
+withDefaults(
+   defineProps<{
+      kicker?: string;
+   }>(),
+   {}
+);
+</script>
