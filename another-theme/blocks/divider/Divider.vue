05c1fce3996f7086fdfe70ca97217ac8c738ab0c
sjmc11 <sjmc11@gmail.com>
2025-12-15 12:10:19 +0000

    Basic alternate theme example

---
+++ another-theme/blocks/divider/Divider.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class="page-container">
+      <hr class="border-t-2 border-emerald-800/30" />
+   </div>
+</template>
+
+<script lang="ts" setup>
+import { defineOptions, defineProps } from "vue";
+
+defineOptions({
+   label: "Divider",
+});
+
+withDefaults(
+   defineProps<{
+      kicker?: string;
+   }>(),
+   {}
+);
+</script>
+
